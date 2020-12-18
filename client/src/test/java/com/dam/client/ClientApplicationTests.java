92c187bbba55dfae4dafc18532f7b386e7da0918
Alessandro D'Amico <damico@intre.it>
2020-12-18 21:59:51 +0100

    04-conf-client

--- client/src/test/java/com/dam/client/ClientApplicationTests.java
+++ client/src/test/java/com/dam/client/ClientApplicationTests.java
@@ -3,7 +3,7 @@ package com.dam.client;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ClientApplicationTests {
 
 	@Test
